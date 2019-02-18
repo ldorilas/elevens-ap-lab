7a6cc2381516623b4e0c6fe3a17b712d18b1b621
ldorilas <43239931+ldorilas@users.noreply.github.com>
2019-02-17 23:35:12 -0500

    Update CardTester.java

--- CardTester.java
+++ CardTester.java
@@ -8,6 +8,10 @@ public class CardTester {
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card card1 = new Card("4", "Spades", 4);
+		Card card2 = new Card("7", "Hearts", 7);
+		Card card3 = new Card("4", "Spades", 4);
+		System.out.println("Does card 1 match card 2?" + card1.matches(card2));
+		System.out.println("Does card 1 match card 3?" + card1.matches(card3));
 	}
 }
